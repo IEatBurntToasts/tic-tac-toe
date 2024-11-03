2954951c10ef3b9a640e14fe26acdebcb990b913
Quan <61860115+Dracozure@users.noreply.github.com>
2024-11-02 23:55:23 -0700

    Add create player factory function

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function createPlayer(name, symbol) {
+    let name = name;
+    let symbol = symbol;
+    let wins = 0;
+
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    const getWins = () => wins;
+    const changeName = (newName) => name = newName;
+    const changeMark = (newSymbol) => symbol = newSymbol;
+    const addWin = () => wins++;
+
+    return { getName, getSymbol, getWins, changeName, changeMark, addWin }
+}
+
 function createTicTacToeBox() {
     let symbol;
 

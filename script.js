40814d2a166239f1e2e826d0fda455d1a7b40b1e
Quan <61860115+Dracozure@users.noreply.github.com>
2024-11-03 00:23:21 -0700

    Delete unnecessary variables in factory function
    Variables that are passed as parameters do not need
    to be reassigned to a new variable inside the function

--- script.js
+++ script.js
@@ -20,23 +20,20 @@ const gameBoard = (function() {
 })();
 
 function createPlayer(name, symbol) {
-    let name = name;
-    let symbol = symbol;
     let wins = 0;
 
     const getName = () => name;
     const getSymbol = () => symbol;
     const getWins = () => wins;
     const changeName = (newName) => name = newName;
-    const changeMark = (newSymbol) => symbol = newSymbol;
+    const changeSymbol = (newSymbol) => symbol = newSymbol;
     const addWin = () => wins++;
 
-    return { getName, getSymbol, getWins, changeName, changeMark, addWin }
+    return { getName, getSymbol, getWins, changeName, changeSymbol, addWin }
 }
 
 function createTicTacToeBox(positionNumber) {
     let symbol;
-    const positionNumber = positionNumber;
 
     const getPositionNumber = () => positionNumber;
     const changeSymbol = (newSymbol) => {
